468e1ade107cc4b54b36eb7f484ea96147fabbed
Tuan Nam <nttnnam@gmail.com>
2022-11-11 15:24:52 +0700

    bai 6

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import config from "@babel/core/lib/config";
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+
+const app = express();
+const port = 3000;
+
+configViewEngine(app);
+
+app.get("/", (req, res) => res.render("index.ejs"));
+
+app.get("/about", (req, res) => res.send("I'm Nam"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
